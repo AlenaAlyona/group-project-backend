f0d23f465b500aa1ebf90277eef5635cc338c41c
farzadtaghavi <fmtaghavi@gmail.com>
2020-10-27 15:52:41 +0100

    created new entity

---
+++ src/entity/Experience.ts
@@ -0,0 +1,52 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Experience {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  education: string;
+
+  @Column()
+  language: string;
+
+  @Column()
+  monthsToJob: number;
+
+  @Column()
+  interviews: string;
+
+  @Column()
+  jobType: string;
+
+  @Column()
+  companySize: string;
+
+  @Column()
+  industry: string;
+
+  @Column()
+  techStack: string;
+
+  @Column()
+  answer1: string;
+
+  @Column()
+  answer2: string;
+
+  @Column()
+  answer3: string;
+
+  @Column()
+  answer4: string;
+
+  @Column()
+  answer5: string;
+
+  @Column()
+  answer6: string;
+
+  @Column()
+  answer7: string;
+}
