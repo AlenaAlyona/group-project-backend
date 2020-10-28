822a521f2081e616e4d767aa7feea6391518a358
svenve-git <svenve@protonmail.com>
2020-10-28 10:12:53 +0100

    set up the login route

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
-import { GetAllExperience } from "./controllers/GetAllExperience";
-import { createGraduate } from "./controllers/CreateGraduate";
+import { GetAllExperience } from "./controllers/GetAllExperience"
+import { createGraduate } from "./controllers/CreateGraduate"
+import { Login } from "./controllers/Login"
 // import { postSaveAction } from "./controllers/PostSaveAction";
 
 /**
@@ -16,9 +17,14 @@ export const AppRoutes = [
     method: "post",
     action: createGraduate,
   },
+  {
+    path: "/login",
+    method: "post",
+    action: Login,
+  },
   // {
   //   path: "/posts",
   //   method: "post",
   //   action: postSaveAction,
   // },
-];
+]
