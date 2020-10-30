1f042051584b776496704d9e29b927e352d936d0
farzadtaghavi <fmtaghavi@gmail.com>
2020-10-30 15:31:34 +0100

    added port

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
-import "reflect-metadata"
-import { createConnection } from "typeorm"
-import { Request, Response } from "express"
-import * as express from "express"
-import * as bodyParser from "body-parser"
-import { AppRoutes } from "./routes"
-const cors = require("cors")
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { Request, Response } from "express";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { AppRoutes } from "./routes";
+const cors = require("cors");
 
-const corsMiddleWare = cors()
-// const PORT = process.env.PORT || 4000
+const corsMiddleWare = cors();
+const PORT: string | number = process.env.PORT || 4000;
 
-console.log("from index.ts:")
+console.log("from index.ts:");
 
 // create connection with database
 // note that it's not active database connection
 // TypeORM creates connection pools and uses them for your requests
 createConnection()
   .then(async (connection) => {
-    console.log("Hey from typeORM")
+    console.log("Hey from typeORM");
     // create express app
-    const app = express()
-    app.use(bodyParser.json())
-    app.use(corsMiddleWare)
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(corsMiddleWare);
     // register all application routes
     AppRoutes.forEach((route) => {
       app[route.method](
@@ -29,14 +29,16 @@ createConnection()
           route
             .action(request, response)
             .then(() => next)
-            .catch((err) => response.status(400).send({ message: err.message }))
+            .catch((err) =>
+              response.status(400).send({ message: err.message })
+            );
         }
-      )
-    })
+      );
+    });
 
     // run app
-    app.listen(4000)
+    app.listen(PORT);
 
-    console.log("Express application is up and running on port 4000")
+    console.log(`Express application is up and running on port ${PORT} `);
   })
-  .catch((error) => console.log("TypeORM connection error: ", error))
+  .catch((error) => console.log("TypeORM connection error: ", error));
